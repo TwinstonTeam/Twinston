c4a0f60a3a62647522e2b8b97bc6d799367e375f
GuyBenBassat <guybb96@gmail.com>
2020-04-04 17:45:30 +0300

    first commit

---
+++ Twinston/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Twinston
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
